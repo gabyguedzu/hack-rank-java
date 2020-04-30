7c7abb98f67d017bea1fde44a7118e4857a654e3
Gabriela Guedes <zooiv3rde@gmail.com>
2020-04-29 22:04:15 -0300

    congrats java if else

--- javaifelse/Solution.java
+++ javaifelse/Solution.java
@@ -1,5 +1,26 @@
 package javaifelse;
 
+import java.util.Scanner;
+
 public class Solution {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numero = input.nextInt();
+
+        if (numero % 2 == 0 && numero >= 2 && numero <= 5) {
+            System.out.println("Not Weird");
+        }
+        else if(numero % 2 == 0 && numero >= 6 && numero <= 20)    {
+            System.out.println("Weird"); 
+        }
+        else if(numero % 2 == 0 && numero > 20) {
+            System.out.println("Not Weird");
+        } else {
+            System.out.println("Weird");
+        }
+        
+        input.close();
+    }
 
 }
